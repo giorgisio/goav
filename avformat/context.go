3dcb3536b0f0f50fe08739d9f55484cc0cf30eed
Vijay Kumar <vijay@ourjamz.com>
2017-11-21 11:27:08 -0800

    Switch back to using AvseekFlagBackward when seeking, this reduces artifacts but A/V sync may be off. Real fix to come later

--- avformat/context.go
+++ avformat/context.go
@@ -119,8 +119,8 @@ func (s *Context) AvSeekFrame(st int, t int64, f int) int {
 // |timebase| is codec specific and can be obtained by calling AvCodecGetPktTimebase2
 func (s *Context) AvSeekFrameTime(st int, at time.Duration, timebase common.AVRational) int {
 	t2 := C.double(C.double(at.Seconds())*C.double(timebase.Den)) / (C.double(timebase.Num))
-	log.Printf("Seeking to time src:%v %v", at, t2)
-	return int(C.av_seek_frame((*C.struct_AVFormatContext)(s), C.int(st), C.int64_t(t2), AvseekFlagAny))
+	log.Printf("Seeking to time :%v TimebaseTime:%v ActualTimebase:%v", at, t2, timebase)
+	return int(C.av_seek_frame((*C.struct_AVFormatContext)(s), C.int(st), C.int64_t(t2), AvseekFlagBackward))
 }
 
 //Seek to timestamp ts.
