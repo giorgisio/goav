6310846765aadedc487afb861a1cd1e457d376b4
Tetsuyuki Kobayashi <tetsu.koba@gmail.com>
2020-05-03 17:47:12 +0900

    Add AvCodecSendFrame() and AvCodecReceivePacket()

--- avcodec/context.go
+++ avcodec/context.go
@@ -202,6 +202,14 @@ func (ctxt *Context) AvcodecReceiveFrame(frame *Frame) int {
 	return (int)(C.avcodec_receive_frame((*C.struct_AVCodecContext)(ctxt), (*C.struct_AVFrame)(frame)))
 }
 
+func (ctxt *Context) AvcodecSendFrame(frame *Frame) int {
+	return (int)(C.avcodec_send_frame((*C.struct_AVCodecContext)(ctxt), (*C.struct_AVFrame)(frame)))
+}
+
+func (ctxt *Context) AvcodecReceivePacket(packet *Packet) int {
+	return (int)(C.avcodec_receive_packet((*C.struct_AVCodecContext)(ctxt), (*C.struct_AVPacket)(packet)))
+}
+
 func (ctxt *Context) SetChannels(channels int) {
 	ctxt.channels = C.int(channels)
 }
