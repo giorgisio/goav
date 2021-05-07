5fcf84ee7579f647e5b0717923f73f0a04d0e99b
Turing Zhu <qishiwenjun@163.com>
2021-05-07 16:58:11 +0800

    [feature] finished avutil change

--- avutil/avutil.go
+++ avutil/avutil.go
@@ -1,5 +1,5 @@
 // Use of this source code is governed by a MIT license that can be found in the LICENSE file.
-// Giorgis (habtom@giorgis.io)
+// Turing Zhu (qishiwenjun@163.com)
 
 // Package avutil is a utility library to aid portable multimedia programming.
 // It contains safe portable string functions, random number generators, data structures,
@@ -16,57 +16,92 @@ import (
 )
 
 type (
-	Options       C.struct_AVOptions
-	AvTree        C.struct_AVTree
-	Rational      C.struct_AVRational
-	MediaType     C.enum_AVMediaType
-	AvPictureType C.enum_AVPictureType
-	PixelFormat   C.enum_AVPixelFormat
+	AVMediaType   C.enum_AVMediaType
+	AVPictureType C.enum_AVPictureType
+	AVRational    C.AVRational
 	File          C.FILE
 )
 
-//Return the LIBAvUTIL_VERSION_INT constant.
-func AvutilVersion() uint {
+// Return the LIBAVUTIL_VERSION_INT constant.
+func AvUtilVersion() uint {
 	return uint(C.avutil_version())
 }
 
-//Return the libavutil build-time configuration.
-func AvutilConfiguration() string {
+// Return an informative version string. This usually is the actual release
+// version number or a git commit description. This string has no fixed format
+// and can change any time. It should never be parsed by code.
+func AvVersionInfo() string {
+	return C.GoString(C.av_version_info())
+}
+
+// Return the libavutil build-time configuration.
+func AvUtilConfiguration() string {
 	return C.GoString(C.avutil_configuration())
 }
 
-//Return the libavutil license.
-func AvutilLicense() string {
+// Return the libavutil license.
+func AvUtilLicense() string {
 	return C.GoString(C.avutil_license())
 }
 
 //Return a string describing the media_type enum, NULL if media_type is unknown.
-func AvGetMediaTypeString(mt MediaType) string {
+func AvGetMediaTypeString(mt AVMediaType) string {
 	return C.GoString(C.av_get_media_type_string((C.enum_AVMediaType)(mt)))
 }
 
-//Return a single letter to describe the given picture type pict_type.
-func AvGetPictureTypeChar(pt AvPictureType) string {
+/**
+ * Return a single letter to describe the given picture type
+ * pict_type.
+ *
+ * @param[in] pict_type the picture type @return a single character
+ * representing the picture type, '?' if pict_type is unknown
+ */
+func AvGetPictureTypeChar(pt AVPictureType) string {
 	return string(C.av_get_picture_type_char((C.enum_AVPictureType)(pt)))
 }
 
-//Return x default pointer in case p is NULL.
+// Return x default pointer in case p is NULL.
 func AvXIfNull(p, x int) {
 	C.av_x_if_null(unsafe.Pointer(&p), unsafe.Pointer(&x))
 }
 
-//Compute the length of an integer list.
-func AvIntListLengthForSize(e uint, l int, t uint64) uint {
-	return uint(C.av_int_list_length_for_size(C.uint(e), unsafe.Pointer(&l), (C.uint64_t)(t)))
+/**
+ * Compute the length of an integer list.
+ *
+ * @param elsize  size in bytes of each list element (only 1, 2, 4 or 8)
+ * @param term    list terminator (usually 0 or -1)
+ * @param list    pointer to the list
+ * @return  length of the list, in elements, not counting the terminator
+ */
+func AvIntListLengthForSize(eachListElement uint, list int, term uint64) uint {
+	return uint(C.av_int_list_length_for_size(C.uint(eachListElement), unsafe.Pointer(&list), (C.uint64_t)(term)))
 }
 
-//Open a file using a UTF-8 filename.
-func AvFopenUtf8(p, m string) *File {
-	f := C.av_fopen_utf8(C.CString(p), C.CString(m))
+/**
+ * Open a file using a UTF-8 filename.
+ * The API of this function matches POSIX fopen(), errors are returned through
+ * errno.
+ */
+func AvFopenUtf8(path, mode string) *File {
+	f := C.av_fopen_utf8(C.CString(path), C.CString(mode))
 	return (*File)(f)
 }
 
-//Return the fractional representation of the internal time base.
-func AvGetTimeBaseQ() Rational {
-	return (Rational)(C.av_get_time_base_q())
+// Return the fractional representation of the internal time base.
+func AvGetTimeBaseQ() AVRational {
+	return (AVRational)(C.av_get_time_base_q())
+}
+
+/**
+ * Fill the provided buffer with a string containing a FourCC (four-character
+ * code) representation.
+ *
+ * @param buf    a buffer with size in bytes of at least AV_FOURCC_MAX_STRING_SIZE
+ * @param fourcc the fourcc to represent
+ * @return the buffer in input
+ */
+func AvFourccMakeString(buf string, fourcc uint32) string {
+	// return C.GoString(C.av_version_info())
+
+	return C.GoString(C.av_fourcc_make_string(C.CString(buf), C.uint32_t(fourcc)))
 }
--- example/versions.go
+++ example/versions.go
@@ -21,7 +21,7 @@ func main() {
 	log.Printf("AvFilter Version:\t%v", avfilter.AvfilterVersion())
 	log.Printf("AvDevice Version:\t%v", avdevice.AvdeviceVersion())
 	log.Printf("SWScale Version:\t%v", swscale.SwscaleVersion())
-	log.Printf("AvUtil Version:\t%v", avutil.AvutilVersion())
+	log.Printf("AvUtil Version:\t%v", avutil.AvUtilVersion())
 	log.Printf("AvCodec Version:\t%v", avcodec.AvcodecVersion())
 	log.Printf("Resample Version:\t%v", swresample.SwresampleLicense())
 
